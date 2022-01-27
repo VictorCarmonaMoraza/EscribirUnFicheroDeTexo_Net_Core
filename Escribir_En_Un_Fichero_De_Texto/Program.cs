5da237f7192f2a7176e0e04193e9a4e7a5484adc
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-27 07:53:30 +0100

    2-Escritura en el fichero y comprobacion de excepciones

--- Escribir_En_Un_Fichero_De_Texto/Program.cs
+++ Escribir_En_Un_Fichero_De_Texto/Program.cs
@@ -7,26 +7,36 @@ namespace Escribir_En_Un_Fichero_De_Texto
     {
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("Prueba2.txt");
 
-            string[] texto =
+            try
             {
+                //Con esta manera machaca elfichero si ya estaba creado
+                //StreamWriter sw = new StreamWriter("Prueba2.txt");
+                //Con el true continua escribiendo pero no machaca el fichero
+                StreamWriter sw = new StreamWriter("Prueba2.txt", true);
+
+                string[] texto =
+                {
                 "Hola a todos",
                 "Estoy escribiendo",
                 "en diferentes",
                 "lineas"
             };
 
-            //Recorro el texto y voy escribiendo
-            foreach (var linea  in texto)
-            {
-                sw.WriteLine(linea);
+                //Recorro el texto y voy escribiendo
+                foreach (var linea in texto)
+                {
+                    sw.WriteLine(linea);
+                }
+                //Cerramos el fichero
+                sw.Close();
+
+                Console.WriteLine("El fichero ha sido escrito");
             }
-            //Cerramos el fichero
-            sw.Close();
-
-            Console.WriteLine("El fichero ha sido escrito");
-
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
--- README.txt
+++ README.txt
@@ -1,2 +1,4 @@
 1-Creacion del proyecto para escribir en archivos de texto
 
+Hemos escrito en un fichero, tambien hemos controlado las excepciones.
+Es lo basico para escribir en un fichero.
